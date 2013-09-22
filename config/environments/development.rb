6b5587c9c4357a617d7f778d5d1d6fb71498329c
Hannah C. McFarlane <hannah.mcfarlane@gmail.com>
2013-09-22 23:36:15 +1000

    Installing Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,11 @@ Treebook::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+
+  #defines default url options after installing devise 
+  #fromTreehouse
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'
+
 end
+
